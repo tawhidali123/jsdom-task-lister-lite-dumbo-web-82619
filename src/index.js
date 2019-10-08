b03026d72448d3e90ea8ab727b53ab5ad6a2c425
Tawhid Ali <tawhidali126@gmail.com>
2019-10-08 17:58:07 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  let submit = document.querySelector("#create-task-form");
+  let post = document.querySelector("#tasks");
+
+  submit.addEventListener("submit", function(event){
+    event.preventDefault();
+    let text = event.target.value
+
+    let li = document.createElement("li");
+
+    // post.innerHTML(text)
+
+    post.innerText = text;
+    // post.appendChild(holder);
+
+  })
+
+
+
+
+
+
+
+    // let userSubmit = document.querySelector("#create-task-form").value;
+    // let showList = document.querySelector("#tasks")
+
+    // let li = document.createElement("li");
+
+    // userSubmit.append(showList);
+
+    // console.log(userSubmit)
+  
+
+
 });
